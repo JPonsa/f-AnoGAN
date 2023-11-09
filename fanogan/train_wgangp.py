2a27561899270a7965df0694337f512c1430a657
JPonsa <joan.ponsa.cobas@gmail.com>
2023-11-09 15:16:09 +0000

    add tensorboard logging to train_wgangp

--- fanogan/train_wgangp.py
+++ fanogan/train_wgangp.py
@@ -2,13 +2,17 @@ import os
 import torch
 import torch.autograd as autograd
 from torchvision.utils import save_image
+from torch.utils.tensorboard import SummaryWriter
 
 
 """
-These codes are:
+Original code from
 Copyright (c) 2018 Erik Linder-Norén
 Licensed under MIT
 (https://github.com/eriklindernoren/PyTorch-GAN/blob/master/LICENSE)
+
+Modifications:
+- Add tensorboard reporting capabilities
 """
 
 
@@ -17,38 +21,48 @@ def compute_gradient_penalty(D, real_samples, fake_samples, device):
     # Random weight term for interpolation between real and fake samples
     alpha = torch.rand(*real_samples.shape[:2], 1, 1, device=device)
     # Get random interpolation between real and fake samples
-    interpolates = (alpha * real_samples + (1 - alpha) * fake_samples)
+    interpolates = alpha * real_samples + (1 - alpha) * fake_samples
     interpolates = autograd.Variable(interpolates, requires_grad=True)
     d_interpolates = D(interpolates)
     fake = torch.ones(*d_interpolates.shape, device=device)
     # Get gradient w.r.t. interpolates
-    gradients = autograd.grad(outputs=d_interpolates, inputs=interpolates,
-                              grad_outputs=fake, create_graph=True,
-                              retain_graph=True, only_inputs=True)[0]
+    gradients = autograd.grad(
+        outputs=d_interpolates,
+        inputs=interpolates,
+        grad_outputs=fake,
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True,
+    )[0]
     gradients = gradients.view(gradients.shape[0], -1)
     gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
     return gradient_penalty
 
 
-def train_wgangp(opt, generator, discriminator,
-                 dataloader, device, lambda_gp=10):
+def train_wgangp(opt, generator, discriminator, dataloader, device, lambda_gp=10):
     generator.to(device)
     discriminator.to(device)
 
-    optimizer_G = torch.optim.Adam(generator.parameters(),
-                                   lr=opt.lr, betas=(opt.b1, opt.b2))
-    optimizer_D = torch.optim.Adam(discriminator.parameters(),
-                                   lr=opt.lr, betas=(opt.b1, opt.b2))
+    optimizer_G = torch.optim.Adam(
+        generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2)
+    )
+    optimizer_D = torch.optim.Adam(
+        discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2)
+    )
 
     os.makedirs("results/images", exist_ok=True)
+
+    # Create directory to record tensorboard logs
+    os.makedirs("logs/train_wgangp", exist_ok=True)
+
+    writer = SummaryWriter(log_dir="logs/train_wgangp")
 
     padding_epoch = len(str(opt.n_epochs))
     padding_i = len(str(len(dataloader)))
 
     batches_done = 0
     for epoch in range(opt.n_epochs):
-        for i, (imgs, _)in enumerate(dataloader):
-
+        for i, (imgs, _) in enumerate(dataloader):
             # Configure input
             real_imgs = imgs.to(device)
 
@@ -69,13 +83,15 @@ def train_wgangp(opt, generator, discriminator,
             # Fake images
             fake_validity = discriminator(fake_imgs.detach())
             # Gradient penalty
-            gradient_penalty = compute_gradient_penalty(discriminator,
-                                                        real_imgs.data,
-                                                        fake_imgs.data,
-                                                        device)
+            gradient_penalty = compute_gradient_penalty(
+                discriminator, real_imgs.data, fake_imgs.data, device
+            )
             # Adversarial loss
-            d_loss = (-torch.mean(real_validity) + torch.mean(fake_validity)
-                      + lambda_gp * gradient_penalty)
+            d_loss = (
+                -torch.mean(real_validity)
+                + torch.mean(fake_validity)
+                + lambda_gp * gradient_penalty
+            )
 
             d_loss.backward()
             optimizer_D.step()
@@ -84,7 +100,6 @@ def train_wgangp(opt, generator, discriminator,
 
             # Train the generator and output log every n_critic steps
             if i % opt.n_critic == 0:
-
                 # -----------------
                 #  Train Generator
                 # -----------------
@@ -99,17 +114,28 @@ def train_wgangp(opt, generator, discriminator,
                 g_loss.backward()
                 optimizer_G.step()
 
-                print(f"[Epoch {epoch:{padding_epoch}}/{opt.n_epochs}] "
-                      f"[Batch {i:{padding_i}}/{len(dataloader)}] "
-                      f"[D loss: {d_loss.item():3f}] "
-                      f"[G loss: {g_loss.item():3f}]")
+                print(
+                    f"[Epoch {epoch:{padding_epoch}}/{opt.n_epochs}] "
+                    f"[Batch {i:{padding_i}}/{len(dataloader)}] "
+                    f"[D loss: {d_loss.item():3f}] "
+                    f"[G loss: {g_loss.item():3f}]"
+                )
 
                 if batches_done % opt.sample_interval == 0:
-                    save_image(fake_imgs.data[:25],
-                               f"results/images/{batches_done:06}.png",
-                               nrow=5, normalize=True)
+                    save_image(
+                        fake_imgs.data[:25],
+                        f"results/images/{batches_done:06}.png",
+                        nrow=5,
+                        normalize=True,
+                    )
 
                 batches_done += opt.n_critic
+
+        writer.add_scalar("D loss", d_loss, epoch)
+        writer.add_scalar("G loss", g_loss, epoch)
+
+    writer.flush()  # ensure all pending events are written to disk
+    writer.close()
 
     torch.save(generator.state_dict(), "results/generator")
     torch.save(discriminator.state_dict(), "results/discriminator")
