3464dd8e963639d06fd163f86cc69ff6d474d122
JPonsa <joan.ponsa.cobas@gmail.com>
2023-11-10 09:16:56 +0000

    import fanogan from parent dir

--- your_own_dataset/test_anomaly_detection.py
+++ your_own_dataset/test_anomaly_detection.py
@@ -6,18 +6,25 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 
-from fanogan.test_anomaly_detection import test_anomaly_detection
+# Add ".." to import from parent dir
+from ..fanogan.test_anomaly_detection import test_anomaly_detection
 
 
 def main(opt):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    pipeline = [transforms.Resize([opt.img_size]*2),
-                transforms.RandomHorizontalFlip()]
+    pipeline = [
+        transforms.Resize([opt.img_size] * 2),
+        transforms.RandomHorizontalFlip(),
+    ]
     if opt.channels == 1:
         pipeline.append(transforms.Grayscale())
-    pipeline.extend([transforms.ToTensor(),
-                     transforms.Normalize([0.5]*opt.channels, [0.5]*opt.channels)])
+    pipeline.extend(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize([0.5] * opt.channels, [0.5] * opt.channels),
+        ]
+    )
 
     transform = transforms.Compose(pipeline)
     dataset = ImageFolder(opt.test_root, transform=transform)
@@ -30,8 +37,9 @@ def main(opt):
     discriminator = Discriminator(opt)
     encoder = Encoder(opt)
 
-    test_anomaly_detection(opt, generator, discriminator, encoder,
-                           test_dataloader, device)
+    test_anomaly_detection(
+        opt, generator, discriminator, encoder, test_dataloader, device
+    )
 
 
 """
@@ -44,17 +52,26 @@ Licensed under MIT
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("test_root", type=str,
-                        help="root name of your dataset in test mode")
-    parser.add_argument("--force_download", "-f", action="store_true",
-                        help="flag of force download")
-    parser.add_argument("--latent_dim", type=int, default=100,
-                        help="dimensionality of the latent space")
-    parser.add_argument("--img_size", type=int, default=64,
-                        help="size of each image dimension")
-    parser.add_argument("--channels", type=int, default=3,
-                        help="number of image channels (If set to 1, convert image to grayscale)")
+    parser.add_argument(
+        "test_root", type=str, help="root name of your dataset in test mode"
+    )
+    parser.add_argument(
+        "--force_download", "-f", action="store_true", help="flag of force download"
+    )
+    parser.add_argument(
+        "--latent_dim", type=int, default=100, help="dimensionality of the latent space"
+    )
+    parser.add_argument(
+        "--img_size", type=int, default=64, help="size of each image dimension"
+    )
+    parser.add_argument(
+        "--channels",
+        type=int,
+        default=3,
+        help="number of image channels (If set to 1, convert image to grayscale)",
+    )
     opt = parser.parse_args()
 
     main(opt)
